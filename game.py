91c4b342fa984d5568ff1108685a4c2db17f0d43
None Store <help.none.store@gmail.com>
2023-05-18 08:52:21 +0300

    second commit

--- game.py
+++ game.py
@@ -1,21 +1,58 @@
+from random import randint
 import pygame
+from balls import Ball
 
 pygame.init()
+pygame.time.set_timer(pygame.USEREVENT, 2000)
 
-pygame.display.set_mode((600, 400), pygame.RESIZABLE)
+WIDTH, HEIGHT = 600, 400
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+BLUE = (0, 0, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+
+window = pygame.display.set_mode((WIDTH, HEIGHT), pygame.RESIZABLE)
 pygame.display.set_caption("MyGame")
-pygame.display.set_icon(pygame.image.load("icon.bmp"))
-
+pygame.display.set_icon(pygame.image.load("img/icon.bmp"))
 clock = pygame.time.Clock()
 FPS = 60
 
-#running = True
+balls_images = ['ball_1.png', 'ball_2.png']
+balls_surf = [pygame.image.load('img/' + img).convert_alpha() for img in balls_images]
+
+def createBall(group):
+    indx = randint(0, len(balls_surf)-1)
+    x = randint(20, WIDTH-20)
+    speed = randint(1,4)
+
+    return Ball(x, speed, balls_surf[indx], group)
+
+background = pygame.image.load('img/football.jpg').convert()
+balls = pygame.sprite.Group()
+
+speed = 1
+
+createBall(balls)
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit()
-            # pygame.quit()
-            # running = False
-        # pygame.time.delay(20)
-        clock.tick(FPS)
-# print('Done')
+        elif event.type == pygame.USEREVENT:
+            createBall(balls)
+
+    window.blit(background, (0, 0))
+    balls.draw(window)
+    pygame.display.update()
+
+    clock.tick(FPS)
+
+    balls.update(HEIGHT)
+
+
+
+
+
+
